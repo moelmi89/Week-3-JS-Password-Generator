82c4ae5f2f3a1eb5980c8d4b6212b7789d5157d9
Moelmi89 <moelmi89@gmail.com>
2022-10-17 15:12:59 +0100

    addition of variables

--- Assets/script.js
+++ Assets/script.js
@@ -19,5 +19,18 @@ function generatePassword() {
     hasNumberChars: false,
   }
 }
+
+  var passLength = 0;
+  passLength = confirm ("How many characters would you like in your password")
+  if (passLength < 8 || passLength > 120) {
+    alert("invalid password length!")
+    return;
+  }
+
+  var hasLowerChars = prompt("Would you like Lower case letters?")
+  var hasUpperChars = prompt("Would you like Upper case letters?")
+  var hasSpecialChars = prompt("Would you like Specail characters letters?")
+  var hasNumberChars = prompt("Would you like numbers?")
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
